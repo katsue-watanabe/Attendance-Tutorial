41296aedb206b38dad8335a3b733d623dbc5a1e3
katsue.w <katsue.w@outlook.jp>
2021-08-07 08:53:17 +0000

    Finish full-fledged-login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : foget(user)
       redirect_to user
     else
       flash.now[:danger] = '認証に失敗しました。'
@@ -16,7 +16,9 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    log_out
-    flash[:success] = 'ログアウトしました。'
+  # ログイン中の場合のみログアウト処理を実行します。
+  log_out if logged_in?
+  flash[:success] = 'ログアウトしました。'
+  redirect_to root_url
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,12 +6,12 @@ module SessionsHelper
   
   def remember(user)
     user.remember
-    cookies.permanent.sigined[:user_id] = user.id
+    cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
   
   def foget(user)
-    user.foget
+    user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
