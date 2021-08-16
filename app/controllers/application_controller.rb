44b33b84cc6cfbde78ee9dd2147e4b211d0e38ca
katsue.w <katsue.w@outlook.jp>
2021-08-16 08:05:26 +0000

    Finish edit-attendances

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,26 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   $days_of_the_week = %w{日 月 火 水 木 金 土}
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "ログインしてください。"
+      redirect_to login_url
+    end
+  end
+  
+  def correct_user
+    redirect_to(root_url) unless current_user?(@user)
+  end
+  
+  def admin_user
+  redirect_to root_url unless current_user.admin?
+  end
 
   # ページ出力前に1ヶ月分のデータの存在を確認・セットします。
   def set_one_month 
@@ -18,6 +38,7 @@ class ApplicationController < ActionController::Base
         # 繰り返し処理により、1ヶ月分の勤怠データを生成します。
         one_month.each { |day| @user.attendances.create!(worked_on: day) }
       end
+    @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on) 
     end
 
   rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
